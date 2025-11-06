b814336c94c38aa7143e9d7f3b3aea09c45f296e
Casqui01 <prebanal@alumnos.unex.es>
2025-11-06 22:38:02 +0100

    feat: GB05-11 Desarrollar el endpoint sobre modificar datos de usuarios (publico)

--- src/modules/users/schemas/user.schema.ts
+++ src/modules/users/schemas/user.schema.ts
@@ -1,11 +1,28 @@
 import { HydratedDocument } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Playlist } from "../../playlists/schemas/playlist.schema";
+import { Artist } from "../../artists/schemas/artist.schema";
+
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({ versionKey: false })
 export class User {
-	// TODO: Definir User
+    //ID generado por Supabase
+    @Prop({ required: true, unique: true })
+    id: string;
+
+    @Prop()
+    username?: string;
+
+    @Prop()
+    imgUrl?: string;
+
+    @Prop()
+    playlist?: Playlist[];
+
+    @Prop()
+    following?: Artist[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -7,7 +7,31 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-	constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+    constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-	// TODO: Definir lógica de negocio de users/
+	async findOneByEmail(email: string): Promise<User> {
+		const user = await this.userModel.findOne({ email: email });
+		if (!user) throw NotFoundException;
+		return user;
+	}
+
+	async findOneById(id: string): Promise<User> {
+		const user = await this.userModel.findOne({ id: id });
+		if (!user) throw NotFoundException;
+		return user;
+	}
+
+    async updateUser(id: string, dto: UpdateUserDto): Promise<User> {
+        const user = await this.userModel.findByIdAndUpdate({id, ...dto});
+        if (!user) throw new NotFoundException('User not found');
+        return user;
+    }
+
+	async insert(insertedUser: CreateUserDto): Promise<User> {
+		return await this.userModel.create(insertedUser);
+	}
+
+	async remove(id: string): Promise<void> {
+		await this.userModel.findByIdAndDelete(id);
+	}
 }
